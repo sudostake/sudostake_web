42f9f3e2539966d4d9a70f2f6429c65400bca7ee
codemuhammed <codemuhammed@gmail.com>
2023-10-04 12:08:56 +0100

    WIP: filter vault deals

--- app/liquidity_requests/page.tsx
+++ app/liquidity_requests/page.tsx
@@ -8,6 +8,7 @@ import { db } from '../services/firebase_client';
 import PendingLiquidityRequestInfo from '../widgets/pending_request_info';
 import { usePathname, useRouter } from 'next/navigation';
 import { VaultIndex } from '../utils/interface';
+import { FaFilter, FaChevronLeft, FaChevronRight, FaCaretDown } from 'react-icons/fa';
 
 export default function LiquidityRequests() {
   const [vaults, setVaults] = useState<VaultIndex[]>([]);
@@ -21,7 +22,7 @@ export default function LiquidityRequests() {
   useEffect(() => {
     if (pathname === '/liquidity_requests') {
       setToolBarState({
-        title: 'Lend to vault owners',
+        title: 'Vault Deals',
         show_back_nav: false
       });
     }
@@ -37,36 +38,53 @@ export default function LiquidityRequests() {
   }, []);
 
   return (
-    <div className="h-full w-full overflow-y-scroll text-sm lg:text-base py-8 px-2 lg:px-8">
-      {
-        vaults.length > 0 &&
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
-          {vaults.map((vault, index) => {
-            return (
-              <div key={index} className="w-full p-4 border border-current rounded-lg grid grid-cols-1 gap-2">
-                <span className="flex items-center">
-                  <span>Vault ID</span>
-                  <span className="ml-auto">
-                    #{vault.index_number}
+    <div className='flex flex-col h-full'>
+
+      <div className='flex flex-row px-4 lg:px-8 py-4 border-b border-current'>
+        <span className='flex items-center flex-row gap-4 border border-current rounded p-2' role='button'>
+          <FaFilter className="w-5 h-5" />
+          <span className='text-sm lg:text-base font-medium'>Filter List</span>
+          <FaCaretDown className="w-4 h-4" />
+        </span>
+
+        <span className='flex items-center flex-row gap-8 ml-auto'>
+          <FaChevronLeft className="w-4 h-4" role='button' />
+          <span className='font-bold'>1</span>
+          <FaChevronRight className="w-4 h-4" role='button' />
+        </span>
+      </div>
+
+      <div className="flex-grow text-sm lg:text-base py-8 px-2 lg:px-8 overflow-y-scroll">
+        {
+          vaults.length > 0 &&
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+            {vaults.map((vault, index) => {
+              return (
+                <div key={index} className="w-full p-4 border border-current rounded-lg grid grid-cols-1 gap-2">
+                  <span className="flex items-center">
+                    <span>Vault ID</span>
+                    <span className="ml-auto">
+                      #{vault.index_number}
+                    </span>
                   </span>
-                </span>
-                <PendingLiquidityRequestInfo vault_info={vault} />
-                <button onClick={() => { router.push(`/vaults/${vault.id}`) }} className="flex items-center justify-center h-9 mt-4 border border-current rounded-lg hover:ring-2 hover:ring-offset-2 text-xs lg:text-sm lg:font-medium p-2">
-                  View
-                </button>
-              </div>
-            );
-          })}
-        </div>
-      }
+                  <PendingLiquidityRequestInfo vault_info={vault} />
+                  <button onClick={() => { router.push(`/vaults/${vault.id}`) }} className="flex items-center justify-center h-9 mt-4 border border-current rounded-lg hover:ring-2 hover:ring-offset-2 text-xs lg:text-sm lg:font-medium p-2">
+                    View
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        }
 
-      {
-        vaults.length === 0 &&
-        <div className="flex w-full h-full items-center justify-center">
-          <h2 className="flex items-center"><span>No pending liquidity requests</span></h2>
-        </div>
-      }
+        {
+          vaults.length === 0 &&
+          <div className="flex w-full h-full items-center justify-center">
+            <h2 className="flex items-center"><span>No pending liquidity requests</span></h2>
+          </div>
+        }
 
+      </div>
     </div>
   )
 }
