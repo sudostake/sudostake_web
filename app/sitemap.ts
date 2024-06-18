272ce82ae80f0fa80a6f729cc18cea5e3f2e7d6b
codemuhammed <codemuhammed@gmail.com>
2024-06-18 12:22:40 +0100

    update metadata

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,15 +3,15 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.sudostake.com',
+      url: 'https://sudostake.com',
       lastModified: new Date()
     },
     {
-      url: 'https://www.sudostake.com/liquidity_requests',
+      url: 'https://sudostake.com/liquidity_requests',
       lastModified: new Date()
     },
     {
-      url: 'https://www.sudostake.com/governance',
+      url: 'https://sudostake.com/governance',
       lastModified: new Date()
     },
   ]
