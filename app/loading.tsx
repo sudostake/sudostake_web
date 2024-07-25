7d5f37a7f088a88f84422a31fdd41462928bd5af
Palingram <codemuhammed@gmail.com>
2024-07-25 20:50:42 +0000

    update loading icon

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
     // You can add any UI inside Loading, including a Skeleton.
     return (
         <div className="flex w-full h-full items-center justify-center">
-            <h2 className="flex items-center"><FaSpinner className="w-6 h-6 mr-3 spinner" /> <span>Loading...</span></h2>
+            <progress value={75} max={100} />
         </div>
     );
 }
