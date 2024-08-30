338bd31029d656e8bc4fddcd8d96ee9f8dd77d09
Palingram <codemuhammed@gmail.com>
2024-08-30 01:46:59 +0000

    update modal ui for intercepted vault page

--- app/@vault_info_modal/(.)vaults/[id]/page.tsx
+++ app/@vault_info_modal/(.)vaults/[id]/page.tsx
@@ -1,16 +1,34 @@
 'use client'
 
 import Vault from "@/app/vaults/[id]/page";
+import classNames from "classnames";
 import { usePathname } from "next/navigation";
+import { useRouter } from 'next/navigation';
 
 export default function VaultModalView({ params }: { params: { id: string } }) {
     const pathname = usePathname();
+    const router = useRouter();
 
     return (
         <>
+
             {
                 pathname.startsWith('/vaults/') &&
-                <span className='z-30 fixed left-0 right-0 top-0 bottom-0 mx-auto sm:ml-56'>
+                <div role="button" onClick={() => router.back()}
+                    className="fixed inset-0 bg-opacity-80 backdrop-blur-xs z-30" />
+            }
+
+            {
+                pathname.startsWith('/vaults/') &&
+                <span className={
+                    classNames({
+                        "z-30 fixed left-0 right-0 top-0 bottom-0": true,
+                        "shadow-lg shadow-zinc-300 dark:shadow-black": true,
+                        "lg:border border-zinc-300 dark:lg:border-zinc-800": true,
+                        "w-full max-w-3xl mx-auto": true,
+                        "dark:text-zinc-200": true,
+                    })
+                }>
                     <Vault params={{ ...params, intercepted: true }} />
                 </span>
             }
