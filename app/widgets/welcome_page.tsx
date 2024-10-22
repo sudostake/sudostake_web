9e108398e9af33641afca66e70b5cf6c45c85914
Palingram <codemuhammed@gmail.com>
2024-10-22 14:03:35 +0000

    ui updates

--- app/widgets/welcome_page.tsx
+++ app/widgets/welcome_page.tsx
@@ -3,9 +3,19 @@ import Image from "next/image";
 
 export default function WelcomePage() {
     return (
-        <div className="flex flex-col gap-8">
+        <div className="flex flex-col">
             <div className="flex flex-col items-center justify-center w-full min-h-72 bg-zinc-200 dark:bg-zinc-800 px-4 py-8">
-                <h1 className="font-bold text-4xl">SudoStake</h1>
+                <span className="flex flex-row gap-2 items-center justify-center">
+                    <Image
+                        src="/logo.png"
+                        alt="sudostake Logo"
+                        className="invert-0 dark:invert rounded-full"
+                        width={44}
+                        height={44}
+                        priority
+                    />
+                    <h1 className="font-bold text-4xl">SudoStake</h1>
+                </span>
                 <h2>On-chain property rights for everyone</h2>
             </div>
 
@@ -20,7 +30,7 @@ export default function WelcomePage() {
                 </ul>
             </div>
 
-            <h3 className="font-normal text-xl mt-8 px-4 text-center"> Types of rights you can extend to lenders as a vault owner</h3>
+            <h3 className="font-normal text-xl px-4 py-8 border-t border-zinc-300 dark:border-zinc-600 text-center"> Types of rights you can extend to lenders as a vault owner</h3>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
                 <div
                     className={classNames({
@@ -35,7 +45,7 @@ export default function WelcomePage() {
                         "lg:border-b": 3 <= 3 || 0 >= 3 - 3,
                         "max-sm:border-b": 0 === 3 - 1,
                     })}>
-                    <span className="text-green-600">Fixed interest rental</span> <br /> You can allow the lender to claim a fixed amount of staking rewards with optional voting rights that can be exercised by the lender during the rewards claiming process.
+                    <span className="text-green-600 font-bold">Fixed interest rental</span> <br /> You can allow the lender to claim a fixed amount of staking rewards with optional voting rights that can be exercised by the lender during the rewards claiming process.
                 </div>
                 <div
                     className={classNames({
@@ -50,7 +60,7 @@ export default function WelcomePage() {
                         "lg:border-b": 3 <= 3 || 1 >= 3 - 3,
                         "max-sm:border-b": 1 === 3 - 1,
                     })}>
-                    <span className='text-blue-600'>Fixed term rental</span> <br /> You can allow the lender to claim all staking rewards for a fixed duration of time with optional voting rights that can be exercised by the lender during the rewards claiming process.
+                    <span className='text-blue-600 font-bold'>Fixed term rental</span> <br /> You can allow the lender to claim all staking rewards for a fixed duration of time with optional voting rights that can be exercised by the lender during the rewards claiming process.
                 </div>
                 <div
                     className={classNames({
@@ -65,7 +75,7 @@ export default function WelcomePage() {
                         "lg:border-b": 3 <= 3 || 2 >= 3 - 3,
                         "max-sm:border-b": 2 === 3 - 1,
                     })}>
-                    <span className='text-red-600'>Fixed term loan</span> <br /> You can allow the lender to liquidate a fixed amount of your staked tokens when you default on the loan.
+                    <span className='text-red-600 font-bold'>Fixed term loan</span> <br /> You can allow the lender to liquidate a fixed amount of your staked tokens when you default on the loan.
                 </div>
             </div>
 
